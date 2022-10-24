b10d75827d55b2baf9b8216f25f1fced9bc4cdb4
Makson <maksonsantoss4@gmail.com>
2022-10-24 04:28:08 -0300

    feat: faltando as rotas schedules e correção na post e get da /properties

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Schedules_user_properties } from "./schedules_user_properties.entity";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column({ unique: true })
+  password: string;
+
+  @Column()
+  isAdm: boolean;
+
+  @Column()
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany((type) => Schedules_user_properties, (shed) => shed.user, {
+    // Aqui eu informo qual coluna da tabela se relaciona com Shedules
+    eager: true,
+  })
+  schedules: Schedules_user_properties[];
+}
---
+++ src/errors/appError.ts
@@ -0,0 +1,20 @@
+import { Response } from "express";
+
+export class AppError extends Error {
+  statusCode: number;
+  constructor(statusCode: number, message: string) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
+export function handleError(err: AppError, res: Response) {
+  const { statusCode, message } = err;
+
+  return res.status(statusCode).json({
+    status: "error",
+    statusCode,
+    message,
+  });
+}
